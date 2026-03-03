c5724dd6cbdb03e652652b23d8b5ed677ed9891e
Dmitry <d@kozlov.ru>
2026-03-03 13:40:54 +0700

    Install Hub: Updated design

--- Hub/Pages/Help/Install Hub.swift
+++ Hub/Pages/Help/Install Hub.swift
@@ -9,56 +9,78 @@ import SwiftUI
 
 struct InstallationGuide: View {
   enum OS {
-    case macOS, iOS, tvOS, linux, docker, windows
+    case macOS, iOS, docker, windows
+    var title: String {
+      switch self {
+      case .macOS: "macOS / Linux"
+      case .iOS: "iOS / tvOS"
+      case .docker: "Docker"
+      case .windows: "Windows"
+      }
+    }
   }
   @State var os: OS = .macOS
   var body: some View {
-    ScrollView {
-      VStack(alignment: .leading) {
-        Picker("OS", selection: $os.animation()) {
-          Text("macOS").tag(OS.macOS)
-          Text("Linux").tag(OS.linux)
-          Text("Docker").tag(OS.docker)
-          Text("iOS").tag(OS.iOS)
-          Text("tvOS").tag(OS.tvOS)
-          Text("Windows").tag(OS.windows)
-        }.pickerStyle(.segmented).labelsHidden()
-        switch os {
-        case .macOS, .linux:
-          Text("First you need to install Bun").transition(.blurReplace)
-          CodeView("curl -fsSL https://bun.sh/install | bash")
-          Text("Now install hub launcher").transition(.blurReplace)
-          CodeView("git clone https://github.com/v57/hub-launcher\ncd hub-launcher\nbun install")
-          Text("Run").transition(.blurReplace)
-          CodeView("bun run .")
-        case .docker:
-          Text("Docker allows to run Hub server on a virtual machine. Available for macOS, Linux and Windows")
-            .transition(.blurReplace)
-          CodeView("""
+    VStack {
+      switch os {
+      case .macOS:
+        Text("Install [Bun](https://bun.com)").font(.system(size: 16, weight: .medium, design: .rounded))
+          .transition(.blurReplace)
+        CodeView("curl -fsSL https://bun.sh/install | bash")
+        Text("Run").font(.system(size: 16, weight: .medium, design: .rounded))
+          .transition(.blurReplace)
+        CodeView("bunx v57/hub")
+      case .docker:
+        Text("Run").font(.system(size: 16, weight: .medium, design: .rounded))
+          .transition(.blurReplace)
+        CodeView("""
             docker pull v57dev/hub
-            docker run -d -p 1997:1997 --name hub-launcher v57dev/hub
+            docker run -d -p 1997:1997 --name Hub v57dev/hub
             """)
-        case .iOS, .tvOS:
-          Text("Currently you can only start hub server on **macOS**, **Linux**, **Windows** and using **Docker**.\nIn the future any **Hub** App may be used to connect your services")
-            .transition(.blurReplace)
-        case .windows:
-          Text("Windows version wasn't tested so it's better to use Docker installation.")
-            .fontWeight(.medium)
-            .transition(.blurReplace)
-          Text("1. Install [bun](https://bun.com)")
-            .transition(.blurReplace)
-          CodeView(#"powershell -c "irm bun.sh/install.ps1 | iex""#)
-          Text("2. Download [Hub Lite](http://github.com/v57/hub-lite)")
-            .transition(.blurReplace)
-          Text("3. Run from hub-lite folder")
-            .transition(.blurReplace)
-          CodeView("bun install && bun run .")
-          
-        }
-      }.frame(maxWidth: .infinity, alignment: .leading)
-        .safeAreaPadding(.horizontal)
-        .contentTransition(.numericText())
-    }.navigationTitle("Installation Guide")
+        Text("Docker allows to run Hub server on a virtual machine.\nAvailable for macOS, Linux and Windows")
+          .font(.system(size: 12, weight: .medium, design: .rounded))
+          .foregroundStyle(.secondary)
+          .multilineTextAlignment(.center)
+          .transition(.blurReplace)
+      case .iOS:
+        Text("Currently you can only start hub server on\n**macOS**, **Linux**, **Windows** and using **Docker**.\nIn the future **any Hub App** may be used to connect your services")
+          .font(.system(size: 12, weight: .medium, design: .rounded))
+          .foregroundStyle(.secondary)
+          .multilineTextAlignment(.center)
+          .transition(.blurReplace)
+      case .windows:
+        Text("Install [Bun](https://bun.com)")
+          .font(.system(size: 16, weight: .medium, design: .rounded))
+          .transition(.blurReplace)
+        CodeView(title: "Powershell", #"powershell -c "irm bun.sh/install.ps1 | iex""#)
+        Text("Run")
+          .font(.system(size: 16, weight: .medium, design: .rounded))
+          .transition(.blurReplace)
+        CodeView(title: "Powershell", "bunx v57/hub")
+      }
+    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+      .safeAreaInset(edge: .bottom) {
+        HStack(spacing: 4) {
+          button(os: .macOS)
+          button(os: .docker)
+          button(os: .iOS)
+          button(os: .windows)
+        }.lineLimit(1).padding(.bottom, 4)
+      }
+      .safeAreaPadding(.horizontal)
+      .contentTransition(.numericText())
+      .navigationTitle("Installation Guide")
+      .toolbarTitleDisplayMode(.inline)
+  }
+  func button(os: OS) -> some View {
+    Button {
+      withAnimation { self.os = os }
+    } label: {
+      Text(os.title).font(.system(size: 12, weight: .medium, design: .rounded))
+        .foregroundStyle(.red)
+        .padding(.horizontal, 8).padding(.vertical, 4)
+        .background(.red.opacity(self.os == os ? 0.1 : 0), in: .capsule)
+    }.buttonStyle(.plain)
   }
   struct CodeView: View {
     let title: LocalizedStringKey
@@ -70,23 +92,27 @@ struct InstallationGuide: View {
     }
     var body: some View {
       HStack {
-        Text(code).textSelection()
-          .font(.caption)
-          .fontDesign(.monospaced)
-        Spacer()
-        AsyncButton(copied ? "Copied" : "Copy", systemImage: copied ? "checkmark.circle.fill" : "document.on.document.fill") {
+        AsyncButton {
           try await copy()
-        }.labelStyle(.iconOnly)
-          .contentTransition(.symbolEffect)
-      }
-      .padding().overlay(alignment: .topLeading) {
-        Text(title).padding(.leading).padding(.top, 4)
-          .font(.caption2)
-          .foregroundStyle(.green.opacity(0.7))
-      }.foregroundStyle(.green)
-        .background(.black, in: RoundedRectangle(cornerRadius: 6))
-        .padding(.horizontal, -14)
+        } label: {
+          VStack(alignment: .leading, spacing: 4) {
+            header
+            codeView
+          }.padding(8).background(.background, in: RoundedRectangle(cornerRadius: 10))
+        }.buttonStyle(.plain)
+      }.shadow(color: .black.opacity(0.2), radius: 10)
         .transition(.blurReplace)
+    }
+    var codeView: some View {
+      Text(code).font(.system(size: 12, weight: .medium, design: .monospaced))
+        .foregroundStyle(.secondary)
+        .textSelection()
+    }
+    var header: some View {
+      HStack(spacing: 4) {
+        Image(systemName: copied ? "checkmark.circle.fill" : "apple.terminal.fill")
+        Text(copied ? "Copied to clipboard" : title)
+      }.font(.caption2).foregroundStyle(.red.opacity(0.7))
     }
     func copy() async throws {
       code.copyToClipboard()
