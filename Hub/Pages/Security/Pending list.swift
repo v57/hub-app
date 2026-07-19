75d038e9d9024e1718aa53733fd0353e947c3efe
Dmitry <v57dev@proton.me>
2026-07-19 21:08:10 +0700

    Pending list: Replaced List design with Stack

--- Hub/Pages/Security/Pending list.swift
+++ Hub/Pages/Security/Pending list.swift
@@ -16,41 +16,41 @@ struct PendingListView: View {
   @Environment(Hub.self) private var hub
   @HubState(\.hostPending) private var hostPending
   var body: some View {
-    List {
-      Section {
+    ScrollView {
+      LazyVStack {
         Placeholder(image: "shield", title: "Requests", description: """
           Services can't just join your Hub without permission
           You are only allowing them to create api they ask
           If service will need to add another api, it would have to ask again
           Atm there is no way to decline requests or revoke permissions you already gave, so please be careful
           """) { }
-      }
-      ForEach(hostPending.list) { item in
+        ForEach(hostPending.list) { item in
 #if os(tvOS)
-        AsyncButton {
-          try await hub.host.allow(key: item.id, paths: item.pending)
-        } label: {
-          HStack {
-            Text(item.name)
-            Text(item.id).code()
-            Spacer(minLength: 0)
-            Text("Allow")
-          }
-        }.disabled(!hub.host.canManage)
+          AsyncButton {
+            try await hub.host.allow(key: item.id, paths: item.pending)
+          } label: {
+            HStack {
+              Text(item.name)
+              Text(item.id).code()
+              Spacer(minLength: 0)
+              Text("Allow")
+            }
+          }.disabled(!hub.host.canManage)
 #else
-        HStack {
-          VStack(alignment: .leading) {
-            Text(item.name)
-            Text(item.id).code()
-          }.lineLimit(2)
-          Spacer(minLength: 0)
-          if hub.host.canManage {
-            AsyncButton("Allow") {
-              try await hub.host.allow(key: item.id, paths: item.pending)
+          HStack {
+            VStack(alignment: .leading) {
+              Text(item.name)
+              Text(item.id).code()
+            }.lineLimit(2)
+            Spacer(minLength: 0)
+            if hub.host.canManage {
+              AsyncButton("Allow") {
+                try await hub.host.allow(key: item.id, paths: item.pending)
+              }
             }
           }
-        }
 #endif
+        }
       }
     }
   }
